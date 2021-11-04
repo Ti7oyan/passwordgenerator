8986a317f84e17fed06a4ff25a7ace1ac640e2f0
Ti7oyan <ticiano.morvan@protonmail.com>
2021-11-03 22:26:02 -0300

    fixed path and added link to Github icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ import { FaGithub } from 'react-icons/fa';
 export default function Navbar() {
     return (
         <nav className="fixed top-0 w-full p-3 flex justify-between items-center bg-white border-b-2 border-blue-50">
-            <p className="text-2xl font-light underline">Simple password generator</p>
+            <p className="text-gray-500 font-light"><i className="text-black not-italic text-2xl underline mr-2">Simple password generator</i> by Ticiano Morvan</p>
             <ul>
-                <li className="text-2xl"><FaGithub /></li>
+                <li className="text-2xl"><a href="https://github.com/Ti7oyan/passwordgenerator" target="_blank"><FaGithub className="transition duration-300 hover:text-blue-500" /></a></li>
             </ul>
         </nav>
     )
